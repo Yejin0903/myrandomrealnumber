5179a6f17278b6abfcd8cf45aaa13227006f7734
JUDY <s2018s05@e-mirim.hs.kr>
2020-05-19 19:26:47 +0900

    0519 실습일지_05

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,7 +10,27 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int rnum = random.Next() % 100;
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요 : ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (rnum == num)
+                {
+                    Console.WriteLine("정답입니다!");
+                    break;
+                }
+                else if (rnum > num)
+                {
+                    Console.WriteLine(num + "보다 큰 숫자입니다");
+                }
+                else
+                {
+                    Console.WriteLine(num + "보다 작은 숫자입니다");
+                }
+            }
         }
     }
 }
